b42f3b524dce8eb448b9f1f22c74dc5f76269c2d
Rovshen <rovshenhojayev1@gmail.com>
2024-09-20 15:43:55 +0300

    It's Second changes on code

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     final materialApp = MaterialApp(
       theme: theme,
-      home: Text('Flutter Demo Home Page'), // Todo ...,
+      home: Text('it\'s second change on code base'), // Todo ...,
     );
     return materialApp;
   }
